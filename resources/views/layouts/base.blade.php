eb26d25c3fa9e0edc4942f6a2f89515e07b6da49
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2025-09-14 17:49:48 +0200

    asd

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- Favicon --}}
     <link rel="shortcut icon" href="{{ asset('public/favicon.png') }}" type="image/x-icon">
