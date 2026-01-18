b3e85b475157e95e07179082edc6c37177029ee9
Cristian-Lazzari <cristian.lazzari.cl@gmail.com>
2026-01-18 01:54:41 +0100

    asx

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -52,7 +52,7 @@ class PageController extends Controller
 
         foreach ($rows as $r) {
             if($r->status == 1 || $r->status == '1'){ 
-                dump($r->status);
+                dump($r->status . ' ' . $r->day);
                 $day = $r->day;
                 $field = $r->field;
                 if (!isset($reserved[$day])) {
